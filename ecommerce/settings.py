c9ac2d1b1265ce2eb573e925d209e7cd0c63af6c
Serhiy Smyslov <serhiy.smyslov@gmail.com>
2021-04-15 11:47:48 +0300

    Add models Product and Category. Create root with media.

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -130,6 +130,12 @@ STATIC_ROOT = BASE_DIR / 'static'
 
 STATIC_URL = '/static/'
 
+# Media files
+
+MEDIA_ROOT = BASE_DIR / 'media', 'static'
+
+MEDIA_URL = '/media/'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
--- shop/models.py
+++ shop/models.py
@@ -1,3 +1,41 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    """Save information about category on platform."""
+    name = models.CharField(max_length=250, unique=True)
+    slug = models.CharField(max_length=250, unique=True)
+    description = models.TextField(blank=True)
+    image = models.ImageField(upload_to="category", blank=True)
+
+    class Meta:
+        ordering = ('name',)
+        verbose_name = 'category'
+        verbose_name_plural = 'categories'
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    """Save information about product on platform."""
+    name = models.CharField(max_length=250, unique=True)
+    slug = models.CharField(max_length=250, unique=True)
+    description = models.TextField(blank=True)
+    category = models.ForeignKey(Category,
+                                 on_delete=models.CASCADE)
+    # Connect objects from class Category
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    image = models.ImageField(upload_to="product", blank=True)
+    stock = models.IntegerField()
+    available = models.BooleanField(default=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ('name',)
+        verbose_name = 'product'
+        verbose_name_plural = 'products'
+
+    def __str__(self):
+        return self.name
